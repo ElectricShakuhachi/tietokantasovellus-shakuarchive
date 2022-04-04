c1a1aba248cc2ce23d5a0cbd73f5b6a276f10de9
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-04-04 03:37:30 +0300

    Fix sql schema and deployed db lacking a column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
-CREATE TABLE compositions (id SERIAL PRIMARY KEY, title TEXT, filename TEXT, difficulty INTEGER, composer TEXT, genre TEXT, instrumentcount INTEGER, views INTEGER);
+CREATE TABLE compositions (id SERIAL PRIMARY KEY, title TEXT, filename TEXT, difficulty INTEGER, composer TEXT, genre TEXT, instrumentcount INTEGER, views INTEGER, uploader TEXT);
 CREATE TABLE genres (id SERIAL PRIMARY KEY, title TEXT);
