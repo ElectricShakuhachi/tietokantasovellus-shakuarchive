db691e79f03e381a252573cb054a904806b45dd3
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-04-04 03:07:43 +0300

    Add Heroku config

---
+++ schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE compositions (id SERIAL PRIMARY KEY, title TEXT, filename TEXT, difficulty INTEGER, composer TEXT, genre TEXT, instrumentcount INTEGER, views INTEGER);
+CREATE TABLE genres (id SERIAL PRIMARY KEY, title TEXT);
