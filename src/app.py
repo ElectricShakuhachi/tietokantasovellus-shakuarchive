51d023f5a2a4335ec8eb2ce2abfe8bf2f1d1ff1e
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-05-01 13:48:15 +0300

    Add debugging printout

--- src/app.py
+++ src/app.py
@@ -64,6 +64,7 @@ def index():
             GROUP BY c.id"
     result = db.session.execute(sql)
     compositions = result.fetchall()
+    flash(f"{compositions}")
     return render_template("index.html", count=len(compositions), compositions=compositions)
 
 @app.route("/composition/<int:id>", methods=["GET"])
