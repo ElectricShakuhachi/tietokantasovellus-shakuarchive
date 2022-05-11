72982700bb298223a562ac88a8ece7ec90d83f43
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-05-12 01:31:37 +0300

    Fix reference to column to correct column name

--- src/app.py
+++ src/app.py
@@ -76,10 +76,10 @@ def view_music(id):
     sql = "SELECT c.id AS id, c.title AS title, c.composer AS composer, \
         c.views as views, c.genre AS genre, c.notation AS notation, \
         AVG(r.rating) AS rating, AVG(d.difficulty) AS difficulty, \
-        u.username as uploader, c.filename as filename, t.tags as tags FROM \
+        u.username as uploader, c.filename as filename, t.tag as tag FROM \
         compositions c, ratings r, difficultyratings d, users u, tags t \
         WHERE r.song_id=c.id AND d.song_id=c.id AND c.user_id=u.id \
-        AND t.song_id=c.id AND c.id=(:id) GROUP BY c.id, u.id"
+        AND t.song_id=c.id AND c.id=(:id) GROUP BY c.id, u.id, t.tag"
     result = db.session.execute(sql, {"id":id})
     music = result.fetchone()
     sql = "SELECT n.note AS note, u.username AS uploader \
